5b6deb060e2349194928e3358ba62576b82c4eae
phamquan92 <phamquan2471992@gmail.com>
2022-10-26 18:10:34 +0700

    cm

---
+++ QRCode.Services/ISerivce/IproductService.cs
@@ -0,0 +1,16 @@
+﻿using QRCode.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QRCode.Services.ISerivce
+{
+    public interface IproductService
+    {
+        IQueryable<product> GetAll();
+        bool CreateNew(product obj);
+        bool Update(product obj);
+        bool Delete(product obj);
+    }
+}
---
+++ QRCode.Services/ServiceImp/productService.cs
@@ -0,0 +1,59 @@
+﻿using QRCode.Core.Domain;
+using QRCode.Repositories.IRepository;
+using QRCode.Services.ISerivce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QRCode.Services.ServiceImp
+{
+    public class productService : IproductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public productService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IQueryable<product> GetAll()
+        {
+            return _unitOfWork.productRepository.GetAll();
+        }
+        public bool CreateNew(product obj)
+        {
+            try
+            {
+                _unitOfWork.productRepository.Add(obj);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool Update(product obj)
+        {
+            try
+            {
+                _unitOfWork.productRepository.Update(obj);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool Delete(product obj)
+        {
+            try
+            {
+                _unitOfWork.productRepository.Delete(obj);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
