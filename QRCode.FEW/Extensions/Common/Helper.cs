b74fea30b84a66f5f7e2d7fea19fe94078f5302a
phamquan92 <phamquan2471992@gmail.com>
2022-10-27 17:49:47 +0700

    cm

---
+++ QRCode.FEW/Extensions/Common/Helper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QRCode.FEW.Extensions.Common
+{
+    public class Helper
+    {
+        public string path_file(string ac_upload)
+        {
+            var extractPath = Path.Combine(@"ClientApp");
+            string[] files = Directory.GetDirectories(extractPath, "*.*", SearchOption.AllDirectories);
+            var file_find = files.Where(t => t.Contains("assets"));
+            var file_find_2 = file_find.Where(t => t.EndsWith(ac_upload));
+            var gt = file_find_2.Select(t => t.Split(ac_upload)[0] + ac_upload);
+            var temp = from a in gt
+                       group a by a into gr
+                       select gr.Key;
+            var any_src = temp.Any(t => t.Contains(@"ClientApp\src\assets"));
+            if (any_src)
+                return Path.Combine(@"ClientApp\src\assets\qr_image", ac_upload);
+            else
+                return temp.FirstOrDefault();
+        }
+    }
+}
