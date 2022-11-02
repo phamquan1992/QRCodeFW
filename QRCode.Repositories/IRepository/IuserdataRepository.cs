3ddf2bd5c60bcca8969921ae7a51ddc2cd2946e7
phamquan1992 <phamquan2471992@gmail.com>
2022-11-02 10:21:20 +0700

    cnm

---
+++ QRCode.Repositories/IRepository/IuserdataRepository.cs
@@ -0,0 +1,11 @@
+﻿using QRCode.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QRCode.Repositories.IRepository
+{
+    public interface IuserdataRepository : IBaseRepository<userdata>
+    {
+    }
+}
---
+++ QRCode.Repositories/Repository/userdataRepository.cs
@@ -0,0 +1,18 @@
+﻿using NHibernate;
+using QRCode.Core.Domain;
+using QRCode.Repositories.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QRCode.Repositories.Repository
+{
+    public class userdataRepository : BaseRepository<userdata>, IuserdataRepository
+    {
+        private readonly ISession _session;
+        public userdataRepository(ISession session) : base(session)
+        {
+            _session = session;
+        }
+    }
+}
