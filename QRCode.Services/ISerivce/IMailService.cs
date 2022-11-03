f0e9e3eaaa55f2e1eefc44d5acce85ec06daea55
phamquan1992 <phamquan2471992@gmail.com>
2022-11-03 08:55:32 +0700

    cm

---
+++ QRCode.Services/ISerivce/IMailService.cs
@@ -0,0 +1,13 @@
+﻿using QRCode.Core.Domain2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRCode.Services.ISerivce
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+    }
+}
