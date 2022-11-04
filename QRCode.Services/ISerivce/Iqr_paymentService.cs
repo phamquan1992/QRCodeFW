29360be25fa672520d3991e74ca9badcb445a79d
phamquan1992 <phamquan2471992@gmail.com>
2022-11-04 17:45:59 +0700

    cm

---
+++ QRCode.Services/ISerivce/Iqr_paymentService.cs
@@ -0,0 +1,13 @@
+﻿using QRCode.Core.Domain;
+using System.Linq;
+
+namespace QRCode.Services.ISerivce
+{
+    public interface Iqr_paymentService
+    {
+        IQueryable<qr_payment> GetAll();
+        bool CreateNew(qr_payment obj);
+        bool Update(qr_payment obj);
+        bool Delete(qr_payment obj);
+    }
+}
