b045ed56301f0fd5799f037d25dfec0b82122ba0
phamquan1992 <phamquan2471992@gmail.com>
2022-11-04 17:43:02 +0700

    cm

---
+++ QRCode.Core/Domain/qr_payment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QRCode.Core.Domain
+{
+    public class qr_payment
+    {
+        public virtual long qrpaymentid { get; set; }
+        public virtual string packcode { get; set; }
+        public virtual string packname { get; set; }
+        public virtual int userid { get; set; }
+        public virtual int qrenterpriseid { get; set; }
+        public virtual DateTime? created_date { get; set; }
+        public virtual int created_by { get; set; }
+        public virtual DateTime? lastcreated_date { get; set; }
+        public virtual int lastcreated_by { get; set; }
+        public virtual DateTime? payment_date { get; set; }
+    }
+}
---
+++ QRCode.Core/Domain/qr_paymentMapping.cs
@@ -0,0 +1,25 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QRCode.Core.Domain
+{
+    public class qr_paymentMapping : ClassMap<qr_payment>
+    {
+        public qr_paymentMapping()
+        {
+            Table("qr_payment");
+            Id(x => x.qrpaymentid).GeneratedBy.Identity();
+            Map(x => x.packcode).Nullable();
+            Map(x => x.packname).Not.Nullable();
+            Map(x => x.userid).Not.Nullable();
+            Map(x => x.qrenterpriseid).Nullable();
+            Map(x => x.payment_date).Nullable();
+            Map(x => x.created_date).Nullable();
+            Map(x => x.created_by).Nullable();
+            Map(x => x.lastcreated_date).Nullable();
+            Map(x => x.lastcreated_by).Nullable();
+        }
+    }
+}
