53f364409c059bbd0b1a3b7b989c46f5e5503ece
phamquan1992 <phamquan2471992@gmail.com>
2022-11-04 17:46:13 +0700

    cm

---
+++ QRCode.Services/ServiceImp/qr_paymentService.cs
@@ -0,0 +1,57 @@
+﻿using QRCode.Core.Domain;
+using QRCode.Repositories.IRepository;
+using QRCode.Services.ISerivce;
+using System;
+using System.Linq;
+
+namespace QRCode.Services.ServiceImp
+{
+    public class qr_paymentService: Iqr_paymentService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public qr_paymentService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IQueryable<qr_payment> GetAll()
+        {
+            return _unitOfWork.qr_paymentRepository.GetAll();
+        }
+        public bool CreateNew(qr_payment obj)
+        {
+            try
+            {
+                return _unitOfWork.qr_paymentRepository.Add(obj);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool Update(qr_payment obj)
+        {
+            try
+            {
+                return _unitOfWork.qr_paymentRepository.Update(obj);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool Delete(qr_payment obj)
+        {
+            try
+            {
+                return _unitOfWork.qr_paymentRepository.Delete(obj);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
