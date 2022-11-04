d0f98273f6ae55ef6ffdb74cca03c6835981ea92
phamquan1992 <phamquan2471992@gmail.com>
2022-11-04 17:45:40 +0700

    cm

--- QRCode.Repositories/Repository/UnitOfWork.cs
+++ QRCode.Repositories/Repository/UnitOfWork.cs
@@ -14,11 +14,12 @@ namespace QRCode.Repositories.Repository
         public IcategoryRepository categoryRepository { get; }
         public Iqr_enterpriseRepository qr_enterpriseRepository { get; }
         public IuserdataRepository userdataRepository { get; }
+        public Iqr_paymentRepository qr_paymentRepository { get; }
         private readonly ISessionFactory _sessionFactory;
         private readonly ITransaction _transaction;
         public ISession Session { get; private set; }
         public UnitOfWork(ISessionFactory sessionFactory, IproductRepository productRepository, IlocationRepository locationRepository, IcategoryRepository categoryRepository,
-            Iqr_enterpriseRepository qr_enterpriseRepository, IuserdataRepository userdataRepository)
+            Iqr_enterpriseRepository qr_enterpriseRepository, IuserdataRepository userdataRepository, Iqr_paymentRepository qr_paymentRepository)
         {
             this._sessionFactory = sessionFactory;
             this.Session = _sessionFactory.OpenSession();
@@ -31,6 +32,7 @@ namespace QRCode.Repositories.Repository
             this.categoryRepository = categoryRepository;
             this.qr_enterpriseRepository = qr_enterpriseRepository;
             this.userdataRepository = userdataRepository;
+            this.qr_paymentRepository = qr_paymentRepository;
         }
         public void Dispose()
         {
---
+++ QRCode.Repositories/Repository/qr_paymentRepository.cs
@@ -0,0 +1,19 @@
+﻿using NHibernate;
+using QRCode.Core.Domain;
+using QRCode.Repositories.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace QRCode.Repositories.Repository
+{
+    public class qr_paymentRepository : BaseRepository<qr_payment>, Iqr_paymentRepository
+    {
+        private readonly ISession _session;
+        public qr_paymentRepository(ISession session) : base(session)
+        {
+            _session = session;
+        }
+    }
+}
