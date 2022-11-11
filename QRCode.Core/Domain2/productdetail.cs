4588615b141c9104c7ab1d6286c775577d57c753
phamquan1992 <phamquan2471992@gmail.com>
2022-11-11 16:46:45 +0700

    cm

--- QRCode.Core/Domain2/productdetail.cs
+++ QRCode.Core/Domain2/productdetail.cs
@@ -15,4 +15,34 @@ namespace QRCode.Core.Domain2
         public bool is_delete { get; set; }
         public string value_ip { get; set; }
     }
+    public class productview
+    {
+        public long qrproductid { get; set; }
+        public string url_img { get; set; }
+        public string name { get; set; }
+        public decimal? price { get; set; }
+        public string des_story { get; set; }
+        public string des_manufactur { get; set; }
+        public string des_pack { get; set; }
+        public string des_element { get; set; }
+        public string des_uses { get; set; }
+        public string des_guide { get; set; }
+        public string des_preserve { get; set; }
+        public string des_startdate { get; set; }
+        public string des_enddate { get; set; }
+        public string additional { get; set; }
+        public decimal enterpriseid { get; set; }
+        public List<productview> list_ref { get; set; }
+    }
+    public class enterprisview
+    {
+        public long qrenterpriseid { get; set; }
+        public string logo { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string tel { get; set; }
+        public string fax { get; set; }
+        public string email { get; set; }
+        public string additional { get; set; }
+    }
 }
