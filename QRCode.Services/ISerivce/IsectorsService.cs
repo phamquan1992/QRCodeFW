f17e072f0362b52a060a04f833bccc5eca9986f0
phamquan1992 <phamquan2471992@gmail.com>
2022-11-14 16:49:43 +0700

    cm

---
+++ QRCode.Services/ISerivce/IsectorsService.cs
@@ -0,0 +1,13 @@
+﻿using QRCode.Core.Domain;
+using System.Linq;
+
+namespace QRCode.Services.ISerivce
+{
+    public interface IsectorsService
+    {
+        IQueryable<sectors> GetAll();
+        bool CreateNew(sectors obj);
+        bool Update(sectors obj);
+        bool Delete(sectors obj);
+    }
+}
---
+++ QRCode.Services/ServiceImp/sectorsService.cs
@@ -0,0 +1,59 @@
+﻿using QRCode.Core.Domain;
+using QRCode.Repositories.IRepository;
+using QRCode.Services.ISerivce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QRCode.Services.ServiceImp
+{
+    public class sectorsService : IsectorsService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public sectorsService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IQueryable<sectors> GetAll()
+        {
+            return _unitOfWork.sectorsRepository.GetAll();
+        }
+        public bool CreateNew(sectors obj)
+        {
+            try
+            {
+                return _unitOfWork.sectorsRepository.Add(obj);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool Update(sectors obj)
+        {
+            try
+            {
+                return _unitOfWork.sectorsRepository.Update(obj);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool Delete(sectors obj)
+        {
+            try
+            {
+                return _unitOfWork.sectorsRepository.Delete(obj);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
