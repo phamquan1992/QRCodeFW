ba081fa3cbdc216f9f263d39b129dbc428e192b8
phamquan1992 <phamquan2471992@gmail.com>
2022-11-19 15:31:35 +0700

    cm

---
+++ QRCode.Core/Domain/qr_his_scan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QRCode.Core.Domain
+{
+    public class qr_his_scan
+    {
+        public virtual int qrhisscanid { get; set; }
+        public virtual string tel { get; set; }
+        public virtual string location { get; set; }
+        public virtual string province { get; set; }
+        public virtual string ip { get; set; }
+        public virtual string osystem { get; set; }
+        public virtual string application { get; set; }
+        public virtual DateTime time_scan { get; set; }
+        public virtual string typecode { get; set; }
+        public virtual int dataid { get; set; }
+    }
+}
---
+++ QRCode.Core/Domain/qr_his_scanMapping.cs
@@ -0,0 +1,25 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QRCode.Core.Domain
+{
+    public class qr_his_scanMapping : ClassMap<qr_his_scan>
+    {
+        public qr_his_scanMapping()
+        {
+            Table("qr_his_scan");
+            Id(x => x.qrhisscanid);
+            Map(x => x.application).Nullable();
+            Map(x => x.ip).Nullable();
+            Map(x => x.location).Nullable();
+            Map(x => x.osystem).Nullable();
+            Map(x => x.province).Nullable();
+            Map(x => x.tel).Nullable();
+            Map(x => x.time_scan).Nullable();
+            Map(x => x.typecode).Nullable();
+            Map(x => x.dataid).Nullable();
+        }
+    }
+}
