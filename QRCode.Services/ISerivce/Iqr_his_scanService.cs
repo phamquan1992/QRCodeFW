8bbc4a543aa2521babd13be4f3e932841ad98701
phamquan1992 <phamquan2471992@gmail.com>
2022-11-19 15:32:59 +0700

    cm

---
+++ QRCode.Services/ISerivce/Iqr_his_scanService.cs
@@ -0,0 +1,13 @@
+﻿using QRCode.Core.Domain;
+using System.Linq;
+
+namespace QRCode.Services.ISerivce
+{
+    public interface Iqr_his_scanService
+    {
+        IQueryable<qr_his_scan> GetAll();
+        bool CreateNew(qr_his_scan obj);
+        bool Update(qr_his_scan obj);
+        bool Delete(qr_his_scan obj);
+    }
+}
---
+++ QRCode.Services/ServiceImp/qr_his_scanService.cs
@@ -0,0 +1,59 @@
+﻿using QRCode.Core.Domain;
+using QRCode.Repositories.IRepository;
+using QRCode.Services.ISerivce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QRCode.Services.ServiceImp
+{
+   public class qr_his_scanService : Iqr_his_scanService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public qr_his_scanService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IQueryable<qr_his_scan> GetAll()
+        {
+            return _unitOfWork.qr_his_scanRepository.GetAll();
+        }
+        public bool CreateNew(qr_his_scan obj)
+        {
+            try
+            {
+                return _unitOfWork.qr_his_scanRepository.Add(obj);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool Update(qr_his_scan obj)
+        {
+            try
+            {
+                return _unitOfWork.qr_his_scanRepository.Update(obj);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool Delete(qr_his_scan obj)
+        {
+            try
+            {
+                return _unitOfWork.qr_his_scanRepository.Delete(obj);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
