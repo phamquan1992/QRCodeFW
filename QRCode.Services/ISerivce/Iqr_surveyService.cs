d7953aa853c1a1f33f7ecb801ab7cb2a0696e5ef
phamquan1992 <phamquan2471992@gmail.com>
2022-11-23 08:23:42 +0700

    cm

---
+++ QRCode.Services/ISerivce/Iqr_surveyService.cs
@@ -0,0 +1,13 @@
+﻿using QRCode.Core.Domain;
+using System.Linq;
+
+namespace QRCode.Services.ISerivce
+{
+    public interface Iqr_surveyService
+    {
+        IQueryable<qr_survey> GetAll();
+        bool CreateNew(qr_survey obj);
+        bool Update(qr_survey obj);
+        bool Delete(qr_survey obj);
+    }
+}
---
+++ QRCode.Services/ServiceImp/qr_surveyService.cs
@@ -0,0 +1,57 @@
+﻿using QRCode.Core.Domain;
+using QRCode.Repositories.IRepository;
+using QRCode.Services.ISerivce;
+using System;
+using System.Linq;
+
+namespace QRCode.Services.ServiceImp
+{
+    public class qr_surveyService : Iqr_surveyService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public qr_surveyService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IQueryable<qr_survey> GetAll()
+        {
+            return _unitOfWork.qr_surveyRepository.GetAll();
+        }
+        public bool CreateNew(qr_survey obj)
+        {
+            try
+            {
+                return _unitOfWork.qr_surveyRepository.Add(obj);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool Update(qr_survey obj)
+        {
+            try
+            {
+                return _unitOfWork.qr_surveyRepository.Update(obj);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool Delete(qr_survey obj)
+        {
+            try
+            {
+                return _unitOfWork.qr_surveyRepository.Delete(obj);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
