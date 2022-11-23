8a1ad4ac41c6d8b378fea66407fad6a60273f8d6
phamquan1992 <phamquan2471992@gmail.com>
2022-11-23 08:22:57 +0700

    cm

---
+++ QRCode.Core/Domain/qr_survey.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QRCode.Core.Domain
+{
+    public class qr_survey
+    {
+        public virtual int qrsurveyid { get; set; }
+        public virtual string name { get; set; }
+        public virtual string code { get; set; }
+        public virtual bool status { get; set; }
+        public virtual string additional { get; set; }
+        public virtual DateTime? start_date { get; set; }
+        public virtual DateTime? end_date { get; set; }
+        public virtual DateTime? created_date { get; set; }
+        public virtual int created_by { get; set; }
+        public virtual DateTime? lastcreated_date { get; set; }
+        public virtual int lastcreated_by { get; set; }
+    }
+}
---
+++ QRCode.Core/Domain/qr_surveyMapping.cs
@@ -0,0 +1,26 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QRCode.Core.Domain
+{
+    public class qr_surveyMapping:ClassMap<qr_survey>
+    {
+        public qr_surveyMapping()
+        {
+            Table("qr_survey");
+            Id(m => m.qrsurveyid).GeneratedBy.Identity();
+            Map(m => m.additional).Nullable();
+            Map(m => m.code).Nullable();
+            Map(m => m.created_by).Nullable();
+            Map(m => m.created_date).Nullable();
+            Map(m => m.end_date).Nullable();
+            Map(m => m.lastcreated_by).Nullable();
+            Map(m => m.lastcreated_date).Nullable();
+            Map(m => m.name).Not.Nullable();
+            Map(m => m.start_date).Nullable();
+            Map(m => m.status).Nullable();
+        }
+    }
+}
